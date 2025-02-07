bd1a9baeb1b40e4011efd4858a4e6d4a6d6c2e37
Espen Ødegaard <esod@esod.no>
2025-02-07 23:43:34 +0100

    add mbp17-01 nixos configuration draft

---
+++ mbp17-01/nixos/configuration.nix
@@ -0,0 +1,237 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "mbp17-01"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Oslo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "no";
+    variant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "no";
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.esod = {
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILEvF7PEBh7El5JdDfpG23V+phQctUK2k3jgZZWx7pX0 esod_ed25519" ];
+    isNormalUser = true;
+    description = "Espen";
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
+    shell = pkgs.fish;
+  }; 
+
+  security.sudo.extraRules= [
+    {  users = [ "esod" ];
+      commands = [
+	{ command = "ALL" ;
+	  options= [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
+	}
+      ];
+    }
+  ];
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Try to fix wifi for MBP2017
+  boot.kernelParams = [ "brcmfmac.feature_disable=0x82000" ];
+  # boot.kernelModules = [ "wl" ];
+  # boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  # boot.blacklistedKernelModules = [ "b43" "bcma" "brcmfmac" ];
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    _1password-cli
+    _1password-gui
+    btop
+    cargo
+    curl
+    dig
+    fish
+    fzf
+    gcc
+    git
+    gnumake
+    google-chrome
+    grim
+    htop
+    kitty
+    logseq
+    mako
+    neovim
+    nerdfonts
+    nodejs_22
+    nvd
+    pciutils
+    ripgrep
+    slurp
+    unzip
+    vim
+    vim
+    wezterm
+    wget
+    wl-clipboard
+  ];
+
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  # Neovim
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  # Shells
+  programs.zsh.enable = true;
+  programs.fish = {
+    enable = true;
+    shellInit = ''
+		  fish_vi_key_bindings
+    '';
+  };
+
+
+  # Sway. See https://wiki.nixos.org/wiki/Sway
+  services.gnome.gnome-keyring.enable = true;
+
+  # enable Sway window manager
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
+
+  # kanshi systemd service
+  systemd.user.services.kanshi = {
+    description = "kanshi daemon";
+    environment = {
+      WAYLAND_DISPLAY="wayland-1";
+      DISPLAY = ":0";
+    }; 
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
+    };
+  };
+
+
+
+  # Docker
+  virtualisation.docker.enable = true;
+
+  # Prometheus Node Exporter
+  services.prometheus.exporters.node = {
+    enable = true;
+    port = 9100;
+    enabledCollectors = [
+      "cpu_vulnerabilities"
+      "ethtool"
+    ];
+    openFirewall = true;
+  };
+
+  # Prometheus Process Exporter
+  services.prometheus.exporters.process = {
+    enable = true;
+    port = 9256;
+    openFirewall = true;
+    settings.process_names = [
+      { name = "{{.Comm}}"; cmdline = [ ".+" ]; }
+    ];
+  };
+
+  # Netdata
+  services.netdata = {
+    enable = true;
+    config = {
+      global = {
+	"memory mode" = "ram";
+	"debug log" = "none";
+	"access log" = "none";
+	"error log" = "syslog";
+      };
+    };
+  };
+
+  # Firewall
+  networking.firewall.allowedTCPPorts = [
+    19999 # netdata
+    6666 # nvim remote testing
+  ];
+
+  # Logseq bruker EOL-versjon av Electron
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-27.3.11"
+  ];
+
+
+  # Enables the 1Password CLI
+  programs._1password = { enable = true; };
+
+  # Enables the 1Password desktop app
+  programs._1password-gui = {
+    enable = true;
+
+    # this makes system auth etc. work properly
+    polkitPolicyOwners = [ "esod" ];
+  };
+
+}

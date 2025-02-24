736be80f13d88c731ba45529bef64943f6e28e2b
Espen Ødegaard <esod@esod.no>
2025-02-24 21:56:25 +0100

    add custom fonts

--- nix-01/nixos/configuration.nix
+++ nix-01/nixos/configuration.nix
@@ -41,22 +41,22 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    neovim
-    git
-    htop
     btop
-    fzf
-    dig
-    ripgrep
-    gnumake
-    unzip
-    gcc
-    nerdfonts
     cargo
-    nodejs_22
+    dig
+    fzf
+    gcc
+    git
+    gnumake
+    htop
+    neovim
+    nodejs_23
     nvd
+    openssl
+    ripgrep
+    unzip
+    vim
+    wget
   ];
 
   # Neovim
@@ -117,27 +117,10 @@
     6666 # nvim remote testing
   ];
 
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  # Fonts
+  fonts.packages = with pkgs; [
+          (nerdfonts.override { fonts = [ "ComicShannsMono" ]; })
+  ];
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "24.11"; # Did you read the comment?
 }

88cc5e45af089c1ef1ca4b77ff0f35dcc6606a8b
Espen Ødegaard <esod@esod.no>
2025-12-01 14:56:50 +0100

    mbp17-01: mDNS via avha

--- mbp17-01/nixos/configuration.nix
+++ mbp17-01/nixos/configuration.nix
@@ -423,4 +423,18 @@
     };
   };
 
+  # mDNS via avahi
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
 }

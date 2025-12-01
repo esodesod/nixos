c06302059147fb47761070b6e29b0ead6dca2331
Espen Ødegaard <esod@esod.no>
2025-12-01 14:55:05 +0100

    tux: added mDNS via avahi

--- tux/nixos/configuration.nix
+++ tux/nixos/configuration.nix
@@ -335,4 +335,17 @@ in
   # also enable cudaSupport
   nixpkgs.config.cudaSupport = true;
 
+  # mDNS via avahi
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  }; 
 }
